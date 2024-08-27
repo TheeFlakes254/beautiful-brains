b1a8d47213feb1a0d2a145320a5d4859c4f00aa5
TheeFlakes254 <mmdennis25@gmail.com>
2024-08-27 20:09:27 +0200

    new updates

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+  import "../app.css";
+</script>
+
+<style>
+  .background-image {
+    background-image: url('/bg 2.png'); /* Replace with your image path */
+    background-size: cover;
+    background-position: center;
+    opacity: 0.5; /* Set the opacity of the background image */
+  }
+
+
+</style>
+
+<div class="relative min-h-screen flex items-center justify-center text-center">
+  <div class="absolute inset-0 background-image"></div>
+  <div class="relative  z-10 text-center">
+    <h1 class="text-2xl md:text-4xl font-bold mb-4 text-blue-700">
+        Help Us Build a World Where <br><span class="text-red-500">Every Child</span> Can Thrive
+    </h1><br/><br/>
+
+    <div class="bg-lime-300 rounded-lg shadow-2xl p-6 inline-block border-2 border-black">
+        <p class="text-lg text-blue-400 md:text-xl mb-4">Support the course, by making a donation</p>
+        <form class="flex items-center space-x-2 space-y-7">
+            <label for="donation" class="text-sm">I want to give</label>
+            <input 
+            type="number"
+            id="donation"
+            placeholder="$"
+            class="px-4 py-2 rounded-full focus:outline-none"
+            />
+
+            <button
+            type="submit"
+            class="bg-red-500 text-white px-6 py-2 rounded-full hover:bg-red-600 focus:outline-none"
+            >Donate</button>
+        </form>
+    </div>
+  </div>
+</div>
---
+++ src/routes/Discover/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  // Import any necessary components or functions
+  import "/src/app.css"
+</script>
+
+<main>
+  <h1 class="text-2xl font-bold">How We Support Your Journey</h1>
+
+  <div class="grid grid-cols-2 gap-4">
+    <div class="bg-gray-100 p-4 rounded-lg">
+      <h2 class="text-lg font-bold">Comprehensive Resources</h2>
+      <p>Access to a wide range of educational information in various formats, including guides, videos, and more.</p>
+      <a href="#" class="text-blue-500 hover:underline">Explore resources</a>
+    </div>
+
+    <div class="bg-pink-100 p-4 rounded-lg">
+      <h2 class="text-lg font-bold">Supportive Community</h2>
+      <p>Connect with other experts and individuals in our online community for personalized support.</p>
+      <a href="#" class="text-blue-500 hover:underline">Join Community</a>
+    </div>
+
+    <div class="bg-yellow-100 p-4 rounded-lg">
+      <h2 class="text-lg font-bold">AI-Powered Assistance</h2>
+      <p>Get instant answers to your questions with our AI-powered virtual assistant.</p>
+      <a href="#" class="text-blue-500 hover:underline">Ask an assistant</a>
+    </div>
+  </div>
+</main>
